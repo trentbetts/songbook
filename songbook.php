8a28db0df015484f4b89596a2e81b92d62e11f66
Trent Betts <trentbetts@gmail.com>
2017-10-21 08:46:20 +1100

    Adding the songbook to the commit

---
+++ songbook.php
@@ -0,0 +1,278 @@
+<?php
+     
+	include_once("pdf_lib/tcpdf/tcpdf.php");
+    include_once("mypdf.php");
+
+class SongBook{
+    // Construction
+    function __construct($inputFile, $outputfile){
+        
+        $this->pdf = new MYPDF();
+        
+        // Setup the return json 
+        $this->return_json = new stdClass();
+        $this->return_json->status = "success";
+        $this->return_json->errors = "";
+        //
+        // Set the File Names
+        //
+        $this->input_file_name = $inputFile;
+        $this->output_file_name = $outputfile . "_debug.pdf";
+
+    }
+    
+    function __destruct()
+    {
+        $this->pdf->Output($this->output_file_name, 'F');
+    }
+    
+
+    private function drawSongChords(){
+        // Initalise the positions
+        $nBotomGap = 20;
+        $nXPos = 5;
+        $nYPos = $this->book_margin_y;
+        if($this->song_layout == $this->LAYOUT_CHORDS_BOTTOM){
+            $this->return_json->errors .= "Got a bottom~";
+            $nYPos = $this->pdf->getPageHeight()-25;
+        }
+        // Go through the chords
+        for($i=0; $i < sizeof($this->song_chords); $i++){
+            
+            $chord_image_name = "./image/uke_".$this->song_chords[$i].".png";  
+            if(!fileExists($chord_image_name)){
+                $this->return_json->errors .= "Missing Chord Image $chord_image_name~";
+                $chord_image_name = "./image/uke_404.png";  
+            }
+            $this->pdf->Image($chord_image_name,$nXPos,$nYPos,17);
+            // Increment the position    
+            if($this->song_layout == $this->LAYOUT_CHORDS_LEFT)
+                $nYPos +=28;
+            else
+                $nXPos +=$nBotomGap;
+        }
+    }
+    
+    private function outpuTitle(){
+        //
+        // Output the Song Title
+        //
+        $this->pdf->SetFont($this->book_font_face, 'b', 13);
+        $this->pdf->Text($this->book_margin_x_BOTTOM, 5, $this->song_title . " : " . $this->song_artist );	
+        $this->pdf->SetFont($this->book_font_face, '', $this->book_font_size );
+        // And the song number
+        $this->song_count++;
+        $this->pdf->Text($this->pdf->getPageWidth()-15, 5, $this->song_count );	
+    }
+    
+    private function processDirective($line){
+        
+        if(false !== stripos($line,"{ns")){
+            // End the current song
+            $this->endSong();
+            // Start the new song
+            $this->newSong();
+        }
+        else if(false !== stripos($line,"{lb}")){
+            // Add another line
+            $this->song_y = $this->pdf->GetFontSize()+$this->song_y+2;		
+        }
+        // Comments
+        else if(false !== stripos($line,"{comment:")){
+            // Get the comment
+            $pureComment = substr($line,strlen("{comment:"));
+            $pureComment = substr($pureComment,0,strlen($pureComment)-2);
+            $this->song_y += 3;
+            $this->pdf->SetFont($this->song_font_face, 'b', $this->song_font_size);
+            $this->pdf->Text($this->song_x,$this->song_y,$pureComment);	
+            $this->song_y = $this->pdf->GetFontSize()+$this->song_y+2;		
+            $this->pdf->SetFont($this->song_font_face, '', $this->song_font_size);
+        }
+        else if(false !== stripos($line,"{title:")){
+            $pureValue = substr($line,strlen("{title:"));
+            $pureValue = substr($pureValue,0,strlen($pureValue)-2);
+            $this->song_title = $pureValue;
+            if($this->song_title != " "&& $this->song_artist != "")
+                $this->outpuTitle();
+        }
+        else if(false !== stripos($line,"{artist:")){
+            $pureValue = substr($line,strlen("{artist:"));
+            $pureValue = substr($pureValue,0,strlen($pureValue)-2);
+            $this->song_artist = $pureValue;
+            if($this->song_title != " "&& $this->song_artist != "")
+                $this->outpuTitle();
+        }
+        else if(false !== stripos($line,"{font-offset:")){
+            $pureValue = substr($line,strlen("{font-offset:"));
+            $pureValue = substr($pureValue,0,strlen($pureValue)-2);
+            $this->song_font_size += intVal($pureValue);
+            $this->pdf->SetFont($this->song_font_face, '', $this->song_font_size);
+        }
+        else if(false !== stripos($line,"{font-display_mode:")){
+            $pureValue = substr($line,strlen("{font-offset:"));
+            $pureValue = substr($pureValue,0,strlen($pureValue)-2);
+            $this->song_layout = intVal($pureValue);
+            if($this->song_layout == $LAYOUT_CHORDS_BOTTOM)
+                $this->song_x = $this->book_margin_x_BOTTOM;
+            else
+                $this->song_x = $this->book_margin_x_BOTTOM;
+        }
+                
+    }
+    private function processSongLine($line){
+        
+        $inQuote = false;
+        $currentChordName = "";
+        
+        $nX = $this->song_x;
+        
+        for($i = 0; $i < strlen($line); $i++){
+            $current = $line[$i];
+            // Determine if we are in a quote or not
+            if($inQuote == true){
+                $currentChordName .= $current; 
+                if($current == ']'){
+                    $inQuote = false;
+                    //
+                    // Change the colour and output the font
+                    //
+                    $this->pdf->SetTextColor(255, 0, 0);
+                    $this->pdf->Text($nX,$this->song_y,$currentChordName);	
+                    $nX += $this->pdf->GetStringWidth($currentChordName);
+                    $this->pdf->SetTextColor(0, 0, 0);
+                    //
+                    // Add the chords to the arrays
+                    //
+                    $currentChordName = substr($currentChordName,1,strlen($currentChordName)-2);
+                    if(!in_array($currentChordName,$this->song_chords))
+                        $this->song_chords[sizeof($this->song_chords)] = $currentChordName;
+                    if(!in_array($currentChordName,$this->book_chords))
+                        $this->book_chords[sizeof($this->book_chords)] = $currentChordName;
+                    // Reset the current chord
+                    $currentChordName = "";
+                }
+                continue;
+            }else{
+                // Determine if this is the start of a quote
+                if($current == '['){
+                    $currentChordName .= $current; 
+                    $inQuote = true;
+                    continue;
+                }
+                // Standard Song Character
+               $this->pdf->Text($nX,$this->song_y,$current);	
+               $nX += $this->pdf->GetStringWidth($current);
+            }
+            
+        }
+        // Increment the y insertion point
+		$this->song_y = $this->pdf->GetFontSize()+$this->song_y+2;		
+
+            
+    }
+    
+    
+    private function processLine($line){
+        
+        // Check if it is a directive or not
+        if($line[0]=='{')
+            $this->processDirective($line);
+        // Comment
+        else if($line[0]=='#')
+            return;
+        // Standard Line
+        else if(strlen($line)>2)
+            $this->processSongLine($line);
+            
+        
+    }
+    
+    public function processBook(){
+        //
+        // Assume that there is going to be a song and configure the new song
+        //
+        $this->newSong();
+        // Simply open the file and process each line
+        $fp = fopen($this->input_file_name,"rt");
+        while(!feof($fp)){
+            $line = fgets($fp);
+            $this->processLine($line);
+        }
+        fclose($fp);
+        // End the last song 
+        $this->endSong();
+        // Return any errors
+        return $this->return_json;
+    }
+    
+    //Utility Functions
+    public function fileExists($path){
+    	 return (@fopen($path,"r")==true);
+ 	}	
+
+    private function endSong(){
+        // Draw the song chords
+        $this->drawSongChords();
+    }
+    
+    // Reset all of the variables when creating a new song
+    private function newSong(){
+        
+        // Add a page to the pdf
+        $this->pdf->AddPage();
+        // Reset the font
+        $this->song_font_face = $this->book_font_face;
+        $this->song_font_size = $this->book_font_size;
+        $this->pdf->SetFont($this->song_font_face, '', $this->song_font_size);
+        //
+        // Reset the insertion points
+        //
+        $this->song_x = $this->book_margin_x_LEFT;
+        $this->song_y = $this->book_margin_y;
+        // Set the layout
+        $this->song_layout = $this->LAYOUT_CHORDS_LEFT;
+        // Reset the title and artist
+        $this->song_title = "";
+        $this->song_artist = "";
+    }
+    
+    // The pdf object
+    private $pdf;
+    // Arrays for chords
+	private $chord_array = Array();
+	private $all_chord_array = Array();
+    // The return 
+    private $return_json;
+    //
+    // The file information
+    //
+    private $input_file_name = "";
+    private $output_file_name = "";
+    // Chord Image Arrays
+    private $song_chords = array();
+    private $book_chords = array();
+    
+    private $book_font_face = 'helvetica';
+    private $book_font_size = 12;
+    private $song_font_face = 'helvetica';
+    private $song_font_size = 12;
+    
+    private $song_x = 0;
+    private $song_y = 0;
+    private $song_layout; 
+    
+    private $book_margin_x_LEFT = 32;
+    private $book_margin_x_BOTTOM = 5;
+    private $book_margin_y = 18;
+    
+    private $song_title = "";
+    private $song_artist = "";
+    
+    private $LAYOUT_CHORDS_LEFT = 0;
+    private $LAYOUT_CHORDS_BOTTOM = 1;
+    
+    private $song_count = 0;
+    
+}
+
+?>
